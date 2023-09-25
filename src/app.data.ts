97f7638f2beaf062b6f46b7d7ef0278629a916bb
gigih santoso <gigih985042@gmail.com>
2023-09-25 13:38:49 +0700

    first commit

--- src/app.data.ts
+++ src/app.data.ts
@@ -3,14 +3,15 @@ import { Registrations } from "./entities/registration.entity";
 import { User } from "./entities/user.entity";
 import { Category } from "./entities/category.entity";
 import { Candidate } from "./entities/candidate.entity";
+import 'dotenv/config';
 
 const dataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
+    host: "localhost" || process.env.POSTGRES_HOST,
     port: 5432,
-    username: "postgres",
-    password: "Gigih985042@",
-    database: "quick_count_2024",
+    username: "postgres"  || process.env.POSTGRES_USER,
+    password: "Gigih985042@" || process.env.POSTGRES_PASSWORD,
+    database: "quick_count_2024" || process.env.POSTGRES_DATABASE,
     synchronize: true,
     logging: false,
     entities: [Registrations, User, Category, Candidate],
