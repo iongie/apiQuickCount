bb44e1198522973b888ebda5229c518b73c9414f
gigih santoso <gigih985042@gmail.com>
2023-09-25 14:22:20 +0700

    first commit

--- src/app.data.ts
+++ src/app.data.ts
@@ -11,14 +11,14 @@ const dataSource = new DataSource({
     port: 5432,
     username: "postgres"  || process.env.POSTGRES_USER,
     password: "Gigih985042@" || process.env.POSTGRES_PASSWORD,
-    database: "quick_count_2024" || process.env.POSTGRES_DATABASE,
+    database: "quick_count_2024" || "api-quick-count-postgres",
     synchronize: true,
     logging: false,
     entities: [Registrations, User, Category, Candidate],
     subscribers: [],
     migrations: [],
     poolSize: 30000,
-    entitySkipConstructor: false
+    entitySkipConstructor: false,
 })
 
 export {
